9c1d90b2a3c8c324c6497edfdf113b6035c6c658
ayushiguptagupta54-dev <ayushiguptagupta54@gmail.com>
2026-01-31 13:04:54 +0530

    Add files via upload

---
+++ Variable & datatypes.py
@@ -0,0 +1,212 @@
+#  print(34)
+#  print("Hellloo world")
+#  print("Ayushi gupta", "its just a number")
+#   print the name, age, price in flote value
+
+# age = 21
+# price = 45.33
+# name = "Ayush"
+
+# print(" my name is :", name)
+# print("my age is:" , age)
+# print("my price is:", price)
+
+# age2 = age
+# print(age2)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# # use of strings
+# name1 ="AG"
+# name2 ='AG'
+# name3 ='''AG'''
+
+# print(name1)
+# print(name2)
+# print(name3)
+
+# #  datatypes
+# age = 33
+# old = False
+# a = None
+
+# print(type(old))
+# print(type(None))
+
+# #  Addition
+# a = 3
+# b = 4
+# sum = a + b
+# print(sum)
+
+# #  Subtraction
+# a = 10
+# b = 4
+# diff = a - b
+# print(diff)
+
+#  multiline comments of code
+# print("Ayush Gupta")
+# print("Ayush Gupta")
+# print("Ayush Gupta")
+# print("Ayush Gupta")
+
+#  Types of operator : Arithmatic operations
+# a = 10
+# b = 5
+
+# print(a + b) # addition
+# print (a - b) # subtraction 
+# print(a * b) # mutiplication
+# print(a / b) # division 
+# print (a % b) # rerminder
+# print (a ** b) # a^b = b is the power of a 
+
+#  relational operator
+#   a = 50
+# b = 20
+
+# print(a==b) # false
+# print(a!=b) # true
+# print(a>=b) # true 
+# print(a<=b)  #false
+# print(a<b) # false
+# print(a>b) # true
+
+#   Assignment operator
+# num = 10
+# # num = num + 10
+# num += 10
+# print("num :", num)
+
+# num = 10
+# # num = num + 10
+# num -= 10
+# print("num :", num)
+
+# num = 10
+#  # num = num + 10
+# num *= 10
+# print("num :", num)
+
+# num = 10
+# # num = num + 10
+# num /= 10
+# print("num :", num)
+
+# num = 10
+# # num = num + 10
+# num %= 10
+# print("num :", num)
+
+# num = 10
+# # num = num + 10
+# num **= 10
+# print("num :", num)
+
+# logical operator
+# print(not True)
+# print(not False)
+# a = 50
+# b = 30
+# print(not False)
+# print(not (a > b))
+
+# val1 = True 
+# val2 = False 
+# print("AND operator:", val1 and val2)
+# print("OR operator:", val1 or val2)
+
+# val1 = False
+# val2 = False 
+# print("AND operator:", val1 and val2)
+# print("OR operator:", val1 or val2)
+
+# val1 = True 
+# val2 = True
+# print("AND operator:", val1 and val2)
+# print("OR operator:", val1 or val2)
+
+# val1 = True 
+# val2 = False 
+# print("AND operator:", val1 and val2)
+# print("OR operator:", (a == b) or (a > b))
+
+#  Type conversion
+a = 5
+b = 8.34
+print(a + b)
+
+# Type casting
+a = int("3")
+b = 4.35
+
+print (type(a))
+print (a + b)
+
+a = float("3")
+b = 4.35
+
+print (type(a))
+print (a + b)
+
+a = 1.13
+a = str(a)
+
+print(type(a))
+
+# return the output from terminal
+# name = input("Enter your name:")
+# print("You entered", name)
+
+# val = (input("Enter some value:"val))
+# print(type(val), val)
+
+# int("5")
+# val = int(input("Enter some value:"))
+# print(type(val), val)
+
+# int("5")
+# val = float(input("Enter some value:"))
+# print(type(val), val)
+
+
+# #  for example
+# name = input("enter name:")
+# age  = int(input("enter age:"))
+# marks = float(input("enter marks:"))
+
+# print("welcome", name)
+# print("age =", age)
+# print("marks=", marks)
+
+
+# pratice ques 1 = Write a program to input 2 numbers and print their sum?
+
+# first = int(input("Enter first:"))
+# second = int(input("Enter second:"))
+
+# print("sum =", first + second)
+
+# pratice ques 2 = WAP to input side of a square and print its area?
+
+# side = float(input("enter square side:"))
+
+# print ("area =",side ** 2)
+
+# pratice ques 3 = WAP to input 2 floating pont numbers and print their average ?
+
+# b  = float(input("enter second:"))
+# a  = float(input("enter first:"))
+
+
+# print("avg =", (a+b)/2)
+
+# pratice ques 4 = WAP to inputs  2 int numbers, a and b  print true if a is greater than or equal to b. if not print false?
+
+a  = int(input("enter first : "))
+b  = int(input("enter second : "))
+
+print (a >= b)
