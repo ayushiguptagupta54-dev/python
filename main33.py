30a4239b2feb013d79380349865ac2afd050ee4d
ayushiguptagupta54-dev <ayushiguptagupta54@gmail.com>
2026-03-10 22:25:35 +0530

    Add files via upload

--- main33.py
+++ main33.py
@@ -1,95 +1,31 @@
-# Add or remeove set items
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities.add("Helsinki")
-print(cities)
+#  Dictionaries in python
+dic = {
+    "Ayush": "Human being",
+    "spoon": "object"
+}
+print(dic["Ayush"])
+print(dic["spoon"])
+dic = {
+313: "shubham",
+221: "ayush",
+111: "shivam",
+}
+print(dic[111]) 
 
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Helsinki", "Warsaw", "Seoul"}
-cities.update(cities2)
-print(cities)
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities.remove("Tokyo")
-print(cities)
-
-# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-# cities.remove("Seoul")
-# print(cities)
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-item = cities.pop()
-print(cities)
-print(item)
-
-# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-# del cities
-# print(cities)
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities.clear()
-print(cities)
-
-info = {"Carla", 19, False, 5.9}
-if "Carla" in info:
-    print("Carla is present.")
-else:
-    print("Carla is absent.")
-
-# Joining sets
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
-cities3 = cities.union(cities2)
-print(cities3)
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
-cities3 = cities.intersection(cities2)
-print(cities3)
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
-cities3 = cities.symmetric_difference(cities2)
-print(cities3)
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Seoul", "Kabul", "Delhi"}
-cities3 = cities.difference(cities2)
-print(cities3)
-
-# Set methods
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
-print(cities.isdisjoint(cities2))
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Seoul", "Kabul"}
-print(cities.issuperset(cities2))
-cities3 = {"Seoul", "Madrid", "Kabul"}
-print(cities.issuperset(cities3))
-
-cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
-cities2 = {"Delhi", "Madrid"}
-print(cities2.issubset(cities))
+# info  = {'name': 'kaju', 'age': 2}
+# print(info['name'])
+# print(info['age'])
 
 
+info  = {'name': 'kaju', 'age': 2}
+print(info)
+print(info.keys())
+print(info.values())
 
+for key in info.keys():
+    print(f"{key}: {info[key]}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    info  = {'name': 'kaju', 'age': 2}
+print(info.items())
+for key , values in info.items():
+    print(f"The value corresponding to the key {key} is {values}")
